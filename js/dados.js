5b5058aee2f99909ef8bec07d2a7388e287ce84b
luiza <luizacarla2004@gmail.com>
2025-05-27 16:24:35 -0300

    Navbar responsiva

--- js/dados.js
+++ js/dados.js
@@ -108,3 +108,18 @@ fetch('../projetos.json')
 }
 
 carregarDados();
+
+const toggle = document.querySelector('.navbar__toggle');
+const menu = document.querySelector('.navbar__lista');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  const isClickInside = menu.contains(event.target) || toggle.contains(event.target);
+
+  if (!isClickInside) {
+    menu.classList.remove('active');
+  }
+});
