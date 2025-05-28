99bc55e5ac81c64d6a69c205632ef5c46ffb060f
luiza <luizacarla2004@gmail.com>
2025-05-28 16:37:35 -0300

    Ajustando

--- js/ajustar-secao.js
+++ js/ajustar-secao.js
@@ -1,6 +1,6 @@
 function ajustarAlturaInicio(){
     const inicio = document.querySelector('.container');
-    container.style.height = `${window.innerHeight}px`;
+    inicio.style.height = `${window.innerHeight}px`;
 
     window.addEventListener('load', ajustarAlturaInicio);
     window.addEventListener('resize', ajustarAlturaInicio);
