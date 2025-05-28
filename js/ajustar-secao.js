941491ba40409eaca9688b34d53a321f93e3ce2b
luiza <luizacarla2004@gmail.com>
2025-05-28 16:42:58 -0300

    Ajustando

--- js/ajustar-secao.js
+++ js/ajustar-secao.js
@@ -1,7 +1,8 @@
 function ajustarAlturaInicio(){
     const inicio = document.querySelector('.container');
+    if(inicio){
     inicio.style.height = `${window.innerHeight}px`;
-
-    window.addEventListener('load', ajustarAlturaInicio);
-    window.addEventListener('resize', ajustarAlturaInicio);
+    }
 }
+window.addEventListener('load', ajustarAlturaInicio);
+window.addEventListener('resize', ajustarAlturaInicio);
